f08fcffd8399639a8b72ddbd1f44d6d14459e263
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-12 22:48:44 -0300

    Modulariza Componentes e adiciona variável de foto do github

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,22 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/Main Grid'
+import Box from '../src/components/Box'
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
-
-const Box = styled.div`
-  background: #FFFFFF;
-  border-radius: 8px;
-`;
-
-const MainGrid = styled.main`
-  width: 100%;
-  grid-gap: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 16px;
-  .profileArea{
-    display: none;
-    @media(min-width: 860px) {
-      display: block;
-    }
-  }
-  
-  @media(min-width: 860px) {
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
-  }
-`;
+function ProfileSidebar() {
+  return (
+    <Box>
+        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px'}}/>
+      </Box>
+  )
+}
 
 export default function Home() {
+  const githubUser = 'vinicius-godoy';
+  
   return (
   <MainGrid>
     {/* <Box style="grid-area: profileArea;"> */}
     <div className="profileArea" style={{gridArea: 'profileArea'}}>
-      <Box>
-        <img src="https://github.com/vinicius-godoy.png" />
-      </Box>
+      <ProfileSidebar />
     </div>
     <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
       <Box>
