ff339e9673522ca0edd65f53ce4c1c8f1887334c
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-19 11:16:34 -0300

    Arruma validação de login

--- pages/login.js
+++ pages/login.js
@@ -22,22 +22,24 @@ export default function LoginScreen() {
           <form className="box" onSubmit={(infosDoEvento) =>{
               infosDoEvento.preventDefault() 
               console.log('Usuário: ', githubUser)
-              fetch('https://alurakut.vercel.app/api/login', {
+              if(githubUser != ''){
+                fetch('https://alurakut.vercel.app/api/login', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({ githubUser: githubUser })
-              })
-              .then(async (respostaDoServer) => {
-                const dadosDaResposta = await respostaDoServer.json()
-                const token = dadosDaResposta.token
-                nookies.set(null, 'USER_TOKEN', token, {
-                  path: '/',
-                  maxAge: 86400 * 7
                 })
-                router.push('/')
-              })
+                .then(async (respostaDoServer) => {
+                  const dadosDaResposta = await respostaDoServer.json()
+                  const token = dadosDaResposta.token
+                  nookies.set(null, 'USER_TOKEN', token, {
+                    path: '/',
+                    maxAge: 86400 * 7
+                  })
+                  router.push('/')
+                })
+              }
             }}>
             <p>
               Acesse agora mesmo com seu usuário do <strong>GitHub</strong>!
